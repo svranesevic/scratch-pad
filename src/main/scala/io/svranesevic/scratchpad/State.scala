d970ee42209015672b22a0a1840da2f7f44ffe7f
svranesevic <svranesevic@users.noreply.github.com>
2022-08-18 20:29:04 +0200

    Add `State` Monad and `StateT` Monad Transformer

---
+++ src/main/scala/io/svranesevic/scratchpad/State.scala
@@ -0,0 +1,156 @@
+package io.svranesevic.scratchpad
+
+final case class State[S, A](run: S => (S, A)) {
+
+  def runEval(initialState: S): A = run(initialState)._2
+  def runState(initialState: S): S = run(initialState)._1
+
+  def map[A2](f: A => A2): State[S, A2] =
+    State { state =>
+      val (s, a) = run(state)
+      s -> f(a)
+    }
+
+  def flatMap[A2](f: A => State[S, A2]): State[S, A2] =
+    State { state =>
+      val (s, a) = run(state)
+      f(a).run(s)
+    }
+
+  def *>[A2](s: => State[S, A2]): State[S, A2] = flatMap(_ => s)
+}
+
+object State {
+
+  def lift[S, A](a: A): State[S, A] = State(s => s -> a)
+  def inspect[S, S2](f: S => S2): State[S, S2] = State(s => s -> f(s))
+  def get[S]: State[S, S] = inspect(identity)
+  def modify[S](f: S => S): State[S, Unit] = State(s => f(s) -> ())
+  def set[S](s: S): State[S, Unit] = State(_ => s -> ())
+}
+
+object StateMain extends App {
+
+  final case class Robot(id: Long, sentient: Boolean, name: String, model: String)
+  final case class Seed(long: Long) {
+    def next: Seed = Seed(long * 6364136223846793005L + 1442695040888963407L)
+  }
+
+  {
+    // Premise: Immutability == Good
+    // Instead of mutating Seed in-place, we return "next Seed" tupled with generated value
+    def nextLong(seed: Seed): (Seed, Long) = (seed.next, seed.long)
+    def nextBoolean(seed: Seed): (Seed, Boolean) = (seed.next, seed.long >= 0L)
+
+    // This requires us to manually thread through Seed state - Error-prone, noisy, and cumbersome
+    def createRobot(seed: Seed): Robot = {
+      val (seed1, id) = nextLong(seed)
+      val (seed2, sentient) = nextBoolean(seed1)
+      val (seed3, isCatherine) = nextBoolean(seed2)
+      val name = if (isCatherine) "Catherine" else "Carlos"
+      val (_, isReplicant) = nextBoolean(seed3)
+      val model = if (isReplicant) "replicant" else "borg"
+      Robot(id, sentient, name, model)
+    }
+    val robot = createRobot(Seed(42L))
+    assert(robot == Robot(id = 42, sentient = false, name = "Catherine", model = "replicant"))
+  }
+
+  {
+    // State Monad to the rescue
+    val nextLong: State[Seed, Long] = State { seed => seed.next -> seed.long }
+    val nextBoolean: State[Seed, Boolean] = nextLong.map(_ >= 0L)
+    val nextName: State[Seed, String] = nextBoolean.map(if (_) "Catherine" else "Carlos")
+    val nextModel: State[Seed, String] = nextBoolean.map(if (_) "replicant" else "borg")
+
+    val createRobot: State[Seed, Robot] =
+      for {
+        id <- nextLong
+        sentient <- nextBoolean
+        name <- nextName
+        model <- nextModel
+      } yield Robot(id, sentient, name, model)
+
+    val robot = createRobot.runEval(Seed(42L))
+    assert(robot == Robot(id = 42, sentient = false, name = "Catherine", model = "replicant"))
+  }
+
+  {
+    // When modifying State yields Monadic result, we can summon State Monad Transformer to deal with this
+    import scala.concurrent.{Future, Await}
+    import scala.concurrent.ExecutionContext.Implicits.global
+    import scala.concurrent.duration.Duration
+
+    final case class AsyncSeed(long: Long) {
+      def next: Future[AsyncSeed] = Future(AsyncSeed(long * 6364136223846793005L + 1442695040888963407L))
+    }
+
+    val nextLong: StateT[Future, AsyncSeed, Long] = StateT(seed => seed.next.map(_ -> seed.long))
+    val nextBoolean: StateT[Future, AsyncSeed, Boolean] = nextLong.map(_ >= 0L)
+    val nextName: StateT[Future, AsyncSeed, String] = nextBoolean.map(if (_) "Catherine" else "Carlos")
+    val nextModel: StateT[Future, AsyncSeed, String] = nextBoolean.map(if (_) "replicant" else "borg")
+
+    val createRobot: StateT[Future, AsyncSeed, Robot] =
+      for {
+        id <- nextLong
+        sentient <- nextBoolean
+        name <- nextName
+        model <- nextModel
+      } yield Robot(id, sentient, name, model)
+
+    val robot = Await.result(createRobot.runEval(AsyncSeed(42L)), Duration.Inf)
+    assert(robot == Robot(id = 42, sentient = false, name = "Catherine", model = "replicant"))
+  }
+
+  {
+    // "Lifting" immutable.Queue enqueue and dequeue to State
+    import scala.collection.immutable.Queue
+
+    def enqueue(x: Int): State[Queue[Int], Unit] = State(_.enqueue(x) -> ())
+    val deque: State[Queue[Int], Option[Int]] = State { state =>
+      state.dequeueOption match {
+        case Some((x, queue)) => queue -> Some(x)
+        case None             => state -> None
+      }
+    }
+
+    val program =
+      for {
+        _ <- enqueue(42)
+        _ <- enqueue(84)
+        _ <- enqueue(108)
+        _ <- enqueue(12)
+        head <- deque
+      } yield head
+
+    val (queue, value) = program.run(Queue.empty)
+    assert(queue == Queue(84, 108, 12))
+    assert(value == Some(42))
+  }
+
+  {
+
+    // Sample Stack impl in terms of State
+    type Stack = List[Int]
+    def push(x: Int): State[Stack, Unit] = State(xs => (x +: xs) -> ())
+    val pop: State[Stack, Option[Int]] = State {
+      case x :: xs => xs -> Some(x)
+      case s @ Nil => s -> None
+    }
+
+    val stackManip: State[Stack, Option[Int]] =
+      for {
+        _ <- push(3)
+        maybeA <- pop
+        maybeB <- pop
+        maybeSum = for {
+          a <- maybeA
+          b <- maybeB
+        } yield a + b
+      } yield maybeSum
+
+    val (stack, value) = stackManip.run(List(7, 6, 5, 4, 3, 2, 1))
+    assert(stack == List(6, 5, 4, 3, 2, 1))
+    assert(value == Some(10))
+  }
+}
---
+++ src/main/scala/io/svranesevic/scratchpad/StateT.scala
@@ -0,0 +1,160 @@
+package io.svranesevic.scratchpad
+
+import scala.concurrent.{ExecutionContext, Future}
+
+final case class StateT[F[_]: Monad, S, A](runF: F[S => F[(S, A)]]) {
+
+  import Monad._
+
+  def run(initialState: S): F[(S, A)] = runF.flatMap(f => f(initialState))
+
+  def runEval(state: S): F[A] = run(state).map(_._2)
+  def runState(state: S): F[S] = run(state).map(_._1)
+
+  def map[A2](f: A => A2): StateT[F, S, A2] =
+    StateT(run(_).map { case (s, a) =>
+      s -> f(a)
+    })
+
+  def flatMap[A2](f: A => StateT[F, S, A2]): StateT[F, S, A2] =
+    StateT(run(_).flatMap { case (s, a) =>
+      f(a).run(s)
+    })
+
+  def *>[A2](s: StateT[F, S, A2]): StateT[F, S, A2] = flatMap(_ => s)
+}
+
+object StateT {
+
+  import Monad._
+
+  def apply[F[_]: Monad, S, A](f: S => F[(S, A)]): StateT[F, S, A] = new StateT[F, S, A](f.liftF[F])
+
+  def lift[F[_]: Monad, S, A](a: A): StateT[F, S, A] = StateT(s => (s -> a).liftF[F])
+  def inspect[F[_]: Monad, S, S2](f: S => S2): StateT[F, S, S2] = StateT(s => (s -> f(s)).liftF[F])
+  def get[F[_]: Monad, S]: StateT[F, S, S] = inspect(identity)
+  def modify[F[_]: Monad, S](f: S => S): StateT[F, S, Unit] = StateT(s => (f(s) -> ()).liftF[F])
+  def set[F[_]: Monad, S](s: S): StateT[F, S, Unit] = StateT(_ => (s -> ()).liftF[F])
+}
+
+// We need basic Monad to bootstrap ourselves for actual Monad Transformer
+trait Monad[F[_]] {
+  def lift[A](a: A): F[A]
+  def flatMap[A, B](a: F[A])(f: A => F[B]): F[B]
+  final def map[A, B](a: F[A])(f: A => B): F[B] = flatMap(a)(a => lift(f(a)))
+}
+
+object Monad {
+
+  implicit class MonadSyntax[A](val a: A) extends AnyVal {
+    def liftF[F[_]: Monad]: F[A] = implicitly[Monad[F]].lift(a)
+  }
+
+  implicit class MonadOpsSyntax[F[_], A](val a: F[A]) extends AnyVal {
+    def map[B](f: A => B)(implicit M: Monad[F]): F[B] = M.map(a)(f)
+    def flatMap[B](f: A => F[B])(implicit M: Monad[F]): F[B] = M.flatMap(a)(f)
+  }
+
+  implicit val futureMonad: Monad[Future] =
+    new Monad[scala.concurrent.Future] {
+      implicit val ec: ExecutionContext = scala.concurrent.ExecutionContext.parasitic
+      override def lift[A](a: A): Future[A] = Future.successful(a)
+      override def flatMap[A, B](a: Future[A])(f: A => Future[B]): Future[B] = a flatMap f
+    }
+
+  implicit val idMonad: Monad[StateTMain.Id] =
+    new Monad[StateTMain.Id] {
+      override def lift[A](a: A): StateTMain.Id[A] = a
+      override def flatMap[A, B](a: StateTMain.Id[A])(f: A => StateTMain.Id[B]): StateTMain.Id[B] = f(a)
+    }
+}
+
+object StateTMain extends App {
+
+  // State Monad in terms of StateT
+  type Id[T] = T
+  type State[S, A] = StateT[Id, S, A]
+
+  object State {
+
+    def apply[S, A](f: S => (S, A)): State[S, A] = new StateT[Id, S, A](f)
+    def lift[S, A](a: A): State[S, A] = StateT.lift(a)
+    def inspect[S, T](f: S => T): State[S, T] = StateT.inspect(f)
+    def get[S]: State[S, S] = StateT.get
+    def modify[S](f: S => S): State[S, Unit] = StateT.modify(f)
+    def set[S](s: S): State[S, Unit] = StateT.set(s)
+  }
+
+  // ========================================================================================================
+
+  final case class Robot(id: Long, sentient: Boolean, name: String, model: String)
+  final case class Seed(long: Long) {
+    def next: Seed = Seed(long * 6364136223846793005L + 1442695040888963407L)
+  }
+
+  {
+    // Premise: Immutability == Good
+    // Instead of mutating Seed in-place, we return "next Seed" tupled with generated value
+    def nextLong(seed: Seed): (Seed, Long) = (seed.next, seed.long)
+    def nextBoolean(seed: Seed): (Seed, Boolean) = (seed.next, seed.long >= 0L)
+
+    // This requires us to manually thread through "next Seek" - Error-prone, noisy, and cumbersome
+    def createRobot(seed: Seed): Robot = {
+      val (seed1, id) = nextLong(seed)
+      val (seed2, sentient) = nextBoolean(seed1)
+      val (seed3, isCatherine) = nextBoolean(seed2)
+      val name = if (isCatherine) "Catherine" else "Carlos"
+      val (_, isReplicant) = nextBoolean(seed3)
+      val model = if (isReplicant) "replicant" else "borg"
+      Robot(id, sentient, name, model)
+    }
+    val robot = createRobot(Seed(42L))
+    assert(robot == Robot(id = 42, sentient = false, name = "Catherine", model = "replicant"))
+  }
+
+  {
+    // State Monad to the rescue
+    val nextLong: State[Seed, Long] = State { seed => seed.next -> seed.long }
+    val nextBoolean: State[Seed, Boolean] = nextLong.map(_ >= 0L)
+    val nextName: State[Seed, String] = nextBoolean.map(if (_) "Catherine" else "Carlos")
+    val nextModel: State[Seed, String] = nextBoolean.map(if (_) "replicant" else "borg")
+
+    val createRobot: State[Seed, Robot] =
+      for {
+        id <- nextLong
+        sentient <- nextBoolean
+        name <- nextName
+        model <- nextModel
+      } yield Robot(id, sentient, name, model)
+
+    val robot = createRobot.runEval(Seed(42L))
+    assert(robot == Robot(id = 42, sentient = false, name = "Catherine", model = "replicant"))
+  }
+
+  // StateT
+  {
+    import scala.concurrent.{Future, Await}
+    import scala.concurrent.ExecutionContext.Implicits.global
+    import scala.concurrent.duration.Duration
+
+    final case class AsyncSeed(long: Long) {
+      def next: Future[AsyncSeed] = Future(AsyncSeed(long * 6364136223846793005L + 1442695040888963407L))
+    }
+
+    val nextLong: StateT[Future, AsyncSeed, Long] = StateT(seed => seed.next.map(_ -> seed.long))
+    val nextBoolean: StateT[Future, AsyncSeed, Boolean] = nextLong.map(_ >= 0L)
+    val nextName: StateT[Future, AsyncSeed, String] = nextBoolean.map(if (_) "Catherine" else "Carlos")
+    val nextModel: StateT[Future, AsyncSeed, String] = nextBoolean.map(if (_) "replicant" else "borg")
+
+    val createRobot: StateT[Future, AsyncSeed, Robot] =
+      for {
+        id <- nextLong
+        sentient <- nextBoolean
+        name <- nextName
+        model <- nextModel
+      } yield Robot(id, sentient, name, model)
+
+    val robot = Await.result(createRobot.runEval(AsyncSeed(42L)), Duration.Inf)
+    assert(robot == Robot(id = 42, sentient = false, name = "Catherine", model = "replicant"))
+  }
+}
