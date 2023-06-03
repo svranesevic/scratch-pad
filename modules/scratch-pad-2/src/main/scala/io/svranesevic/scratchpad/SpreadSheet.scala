58e998ffe1cbfade7b3ae5f8dbacbecebf22c714
svranesevic <svranesevic@users.noreply.github.com>
2023-06-03 19:39:47 +0200

    Spreadsheet: Decouple operations from evaluation

--- modules/scratch-pad-2/src/main/scala/io/svranesevic/scratchpad/SpreadSheet.scala
+++ modules/scratch-pad-2/src/main/scala/io/svranesevic/scratchpad/SpreadSheet.scala
@@ -1,15 +1,9 @@
 package io.svranesevic.scratchpad
 
-import zio.Console._
-import zio._
-
-import scala.collection.mutable
-
 trait Spreadsheet {
   def cols: Int
   def rows: Int
 
-  def evaluate(cell: Cell): Value
   def evaluate(col: Int, row: Int): Value
 
   def update(cell: Cell): Unit
@@ -32,15 +26,16 @@ trait Spreadsheet {
 
 object Spreadsheet {
   def apply(): Spreadsheet = new Spreadsheet {
+    import scala.collection.mutable
+
     override def cols: Int = Int.MaxValue
     override def rows: Int = Int.MaxValue
 
     val map: mutable.Map[(Int, Int), Cell] = collection.mutable.Map.empty
 
     override protected def cellAt(col: Int, row: Int): Cell =
-      map.getOrElse(((col, row)), Cell(col, row, Content.empty()))
+      map.getOrElse(((col, row)), Cell(col, row))
 
-    override def evaluate(cell: Cell): Value         = evaluate(cell.col, cell.row)
     override def evaluate(col: Int, row: Int): Value = cellAt(col, row).evaluate(this)
 
     override def update(cell: Cell): Unit = map += ((cell.col, cell.row) -> cell)
@@ -53,128 +48,93 @@ object SRange {
   def row(i: Int): SRange    = SRange(Some(i), Some(i), None, None)
 }
 
-final case class Cell(col: Int, row: Int, content: Content) {
-  def evaluate(spreadsheet: Spreadsheet): Value = content.evaluate(spreadsheet)
+final case class Cell(col: Int, row: Int, value: Value, formula: Option[Formula]) {
+  def evaluate(spreadsheet: Spreadsheet): Value =
+    formula match {
+      case None          => value
+      case Some(formula) => formula.evaluate(spreadsheet)
+    }
+
+  def withValue(value: Value): Cell       = copy(value = value, formula = None)
+  def withFormula(formula: Formula): Cell = copy(formula = Some(formula), value = Value.Empty)
 }
 object Cell {
-  def apply(col: Int, row: Int, number: Double): Cell = Cell(col, row, Content(number))
-  def apply(col: Int, row: Int, text: String): Cell   = Cell(col, row, Content(text))
-  def apply(col: Int, row: Int): Cell                 = Cell(col, row, Content.empty())
-}
-
-sealed trait Content {
-  def evaluate(spreadsheet: Spreadsheet): Value =
-    this match {
-      case Content.Literal(value)      => value
-      case Content.Calculated(formula) => formula.evaluate(spreadsheet)
-    }
-}
-object Content {
-  case class Literal(value: Value)        extends Content
-  case class Calculated(formula: Formula) extends Content
-
-  def empty(): Content                 = Content.Literal(Value.Empty)
-  def apply(text: String): Content     = Content.Literal(Value.Text(text))
-  def apply(number: Double): Content   = Content.Literal(Value.Number(number))
-  def apply(formula: Formula): Content = Content.Calculated(formula)
+  def apply(col: Int, row: Int, value: Double): Cell = Cell(col, row, Value.Number(value), formula = None)
+  def apply(col: Int, row: Int, value: String): Cell = Cell(col, row, Value.Text(value), formula = None)
+  def apply(col: Int, row: Int): Cell                = Cell(col, row, Value.Empty, formula = None)
 }
 
 sealed trait Value
 object Value {
-  case object Empty                       extends Value
-  final case class Text(text: String)     extends Value
-  final case class Number(number: Double) extends Value
-  final case class Error(message: String) extends Value
+  case object Empty                           extends Value
+  final case class Text(text: String)         extends Value
+  final case class Number(number: BigDecimal) extends Value
+  final case class Error(message: String)     extends Value
 }
 
 sealed trait Formula {
-  def evaluate(spreadsheet: Spreadsheet): Value
+  def evaluate(spreadsheet: Spreadsheet): Value =
+    this match {
+      case Formula.TextLiteral(value)      => Value.Text(value)
+      case Formula.NumericLiteral(value)   => Value.Number(value)
+      case Formula.CellReference(col, row) => spreadsheet.evaluate(col, row)
+      case Formula.Function(operation, operands) =>
+        val operandsValues = operands.map(_.evaluate(spreadsheet))
+        operation.apply(operandsValues)
+    }
 }
-
 object Formula {
-
-  import Value._
-
-  sealed trait Variable {
-    def evaluate(spreadsheet: Spreadsheet): Seq[Value] =
-      this match {
-        case TextLiteral(text)     => Text(text) :: Nil
-        case NumberLiteral(number) => Number(number) :: Nil
-        case CellValue(col, row)   => spreadsheet.evaluate(col, row) :: Nil
-        case CellsValues(range)    => spreadsheet.scan(range).map(_.evaluate(spreadsheet)).toList.toSeq
-      }
-  }
-  case class TextLiteral(text: String)     extends Variable
-  case class NumberLiteral(number: Double) extends Variable
-  case class CellValue(col: Int, row: Int) extends Variable
-  case class CellsValues(range: SRange)    extends Variable
-
-  final case class Sum(variables: Variable*) extends Formula {
-    override def evaluate(spreadsheet: Spreadsheet): Value = {
-      val values = variables.flatMap(_.evaluate(spreadsheet))
-
-      values.reduce(
-        binaryOp(
-          "Expected values to be numeric",
-          { case (Number(l), Number(r)) =>
-            Number(l + r)
-          }
-        )
-      )
-    }
-  }
-
-  final case class Max(variables: Variable*) extends Formula {
-    override def evaluate(spreadsheet: Spreadsheet): Value = {
-      val values = variables.flatMap(_.evaluate(spreadsheet))
-
-      values.reduce(
-        binaryOp(
-          "Expected values to be numeric",
-          {
-            case (ln @ Number(l), Number(r)) if l >= r => ln
-            case (Number(l), rn @ Number(r)) if l < r  => rn
-          }
-        )
-      )
-    }
-  }
-
-  protected def binaryOp(
-    error: String,
-    f: PartialFunction[(Value, Value), Value]
-  )(left: Value, right: Value): Value =
-    f.lift((left, right)) match {
-      case Some(value) => value
-      case None        => Value.Error(error)
-    }
+  case class TextLiteral(value: String)                              extends Formula
+  case class NumericLiteral(valuet: BigDecimal)                      extends Formula
+  case class CellReference(col: Int, row: Int)                       extends Formula
+  case class Function(operation: Operation, operands: List[Formula]) extends Formula
 }
 
-object SpreadSheetMain extends ZIOAppDefault {
+sealed trait Operation {
+  def apply(values: List[Value]): Value
+}
+object Operation {
+  case object Sum extends Operation {
+    override def apply(values: List[Value]): Value =
+      Value.Number(
+        values
+          .collect { case Value.Number(number) => number }
+          .foldLeft(BigDecimal(0))(_ + _)
+      )
+  }
+}
 
-  override def run = {
-    val spreadsheet = Spreadsheet()
-    val zero        = Cell(0, 0, Content(42))
-    val five        = Cell(5, 5, Content(8))
-    val one         = Cell(1, 1, Content(Formula.Sum(Formula.CellValue(5, 5))))
-    val two = Cell(
-      2,
-      2,
-      Content(
-        Formula.Sum(Formula.CellValue(0, 0), Formula.CellValue(1, 1), Formula.NumberLiteral(100))
+object SpreadSheetMain extends App {
+  val spreadsheet = Spreadsheet()
+  val one         = Cell(col = 1, row = 1, value = 8)
+  val five =
+    Cell(
+      col = 5,
+      row = 5,
+      Value.Empty,
+      Some(
+        Formula.Function(
+          Operation.Sum,
+          List(Formula.CellReference(1, 1))
+        )
       )
     )
-    spreadsheet.update(two)
-    for {
-      _ <- printLine("Spreadsheet")
-      _ <- printLine(s"value(2,2)=${two.evaluate(spreadsheet)}")
-      _ <- ZIO.succeed(spreadsheet.update(zero))
-      _ <- printLine(s"value(2,2)=${two.evaluate(spreadsheet)}")
-      _ <- ZIO.succeed(spreadsheet.update(one))
-      _ <- printLine(s"value(2,2)=${two.evaluate(spreadsheet)}")
-      _ <- ZIO.succeed(spreadsheet.update(five))
-      _ <- printLine(s"value(2,2)=${two.evaluate(spreadsheet)}")
-      _ <- ZIO.sleep(10.seconds)
-    } yield ()
-  }
+  val six = Cell(
+    col = 6,
+    row = 6,
+    Value.Empty,
+    Some(
+      Formula.Function(
+        Operation.Sum,
+        List(Formula.CellReference(0, 0), Formula.CellReference(5, 5), Formula.NumericLiteral(100))
+      )
+    )
+  )
+
+  spreadsheet.update(one)
+  spreadsheet.update(five)
+  spreadsheet.update(six)
+
+  println(s"(5, 5) = ${spreadsheet.evaluate(5, 5)}")
+  println(s"(6, 6) = ${spreadsheet.evaluate(6, 6)}")
 }
